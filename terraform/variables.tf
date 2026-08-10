d5058cbfbc1922db47562a5c220284fdfecd0561
Marco <marco782005@gmail.com>
2026-08-10 22:53:53 +0300

    adding terraform and ansible files

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "my_public_ip" {
+  description = "Your public IP address"
+  type        = string
+}
+
+variable "ubuntu_ami" {
+  description = "Ubuntu Server 26.04 LTS AMI ID"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the existing AWS key pair"
+  type        = string
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "availability_zone" {
+  default = "us-east-1a"
+}
