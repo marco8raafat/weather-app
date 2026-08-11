2f91aa4663c95dc4a87ff65737253f763a9d7a37
Menna Hamouda <mennahamouda2001.com>
2026-08-11 23:59:18 +0300

    Allow HTTP/HTTPS/NodePorts in Security Groups and use baremetal NGINX ingress

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -8,6 +8,20 @@ resource "aws_security_group" "bastion_sg" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port = 0
     to_port   = 0
@@ -25,6 +39,30 @@ resource "aws_security_group" "kubernetes_sg" {
     description     = "SSH from Bastion"
     from_port       = 22
     to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  ingress {
+    description     = "HTTP from Bastion"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  ingress {
+    description     = "HTTPS from Bastion"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  ingress {
+    description     = "NodePorts from Bastion"
+    from_port       = 30000
+    to_port         = 32767
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
   }
